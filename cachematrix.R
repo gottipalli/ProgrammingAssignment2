bd0c0fc51f45522d21782a3ec37056b44434a04d
gottipalli <gottipalli@gmail.com>
2015-05-24 12:45:06 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## makeCacheMatrix:: takes matrix as argument
+## validates mtraix as squared and saves the matrix and its inveverse
+## set, get, getinverse and setinverse are inner functions
+makeCacheMatrix <- function(m = matrix()) {
+  ## Return a list contcaings the internal representationof cached matricx
+  ## m is mtraics, im is inverse matrix stored in parent env
+  if ( nrow(m) != ncol(m)) {
+    message("This is matrix is not squared")
+    return(NULL)
+  }
+  im <- NULL
+  set <- function(y) {
+    m <<- y
+    im <<- NULL
+  }
+  get <- function() {
+    m
+  }
+  setinverse <- function(inverse) {
+    im <<- inverse
+  }
+  getinverse <- function() {
+    im
+  }
+  ## return list with all the functions
+  list(set = set, 
+       get = get, 
+       setinverse = setinverse, 
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## cacheSolve::cacheSolve takes the modified matrix and returns the inverse
+## On 1st call calculates the inverse and stores it in cache.  
+## next call returnes the saved inverse from cache
+cacheSolve <- function(m, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  im <- m$getinverse()
+  if(!is.null(im)) {
+    message("getting cached inverse")
+    return(im)
+  }
+  data <- m$get()
+  im <- solve(data,...)
+  m$setinverse(im)
+  im
 }
+
+# Test the functions
+m1 <-matrix(1:4,2)
+m1
+mv1 <- makeCacheMatrix(m1)
+mv1$get()
+minv1 <- cacheSolve(mv1)
+minv1
+m1%*minv1
